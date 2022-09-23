2a30d094e3a19091bc33cdb8d5110c2132cf4708
dinghino <dinghino@gmail.com>
2022-09-23 17:48:17 +0200

    ⬆️ bump mantine v5.4.0 & update components

--- src/components/overlay/StatsPanel.tsx
+++ src/components/overlay/StatsPanel.tsx
@@ -1,29 +1,23 @@
-import { Card, Text } from "@mantine/core";
+import { Affix, Card, Text } from "@mantine/core";
 import store from "../../data/store";
 
 const StatsPanel = () => {
-    const actualScale = store.useState((s) => s.userSettings.actualScale);
-    const showDebugInfo = store.useState((s) => s.userSettings.showDebugInfo);
-    const cameraDistance = store.useState((s) => s.appSettings.cameraDistance);
-
-    if (!actualScale) {
-        return null;
-    }
-
-    const distance = cameraDistance.toLocaleString(undefined, { maximumFractionDigits: 0 });
-
-    return (
-        <Card
-            p="xs"
-            style={{
-                position: "absolute",
-                top: "6px",
-                left: showDebugInfo ? "85px" : "6px",
-                zIndex: Number.MAX_SAFE_INTEGER,
-            }}
-        >
-            <Text size="sm">{ distance } km</Text>
-        </Card>
+  const actualScale = store.useState((s) => s.userSettings.actualScale);
+  const showDebugInfo = store.useState((s) => s.userSettings.showDebugInfo);
+  const cameraDistance = store.useState((s) => s.appSettings.cameraDistance);
+  
+  if (!actualScale) {
+    return null;
+  }
+  
+  const distance = cameraDistance.toLocaleString(undefined, { maximumFractionDigits: 0 });
+  
+  return (
+    <Affix position={{top: 16, left: showDebugInfo ? 85 : 6 }}>
+      <Card p="xs" style={{ zIndex: Number.MAX_SAFE_INTEGER }}>
+        <Text size="sm">{ distance } km</Text>
+      </Card>
+    </Affix>
     );
 };
 
